91bb15337e8209840fe947f373cfab6db984e8a0
mwendaB <brian.mwenda@student.moringaschool.com>
2021-11-07 20:07:57 +0300

    write the clssess

--- tests/test_comment.py
+++ tests/test_comment.py
@@ -2,4 +2,6 @@ import unittest
 from app.db_class import Comment ,Pitch
 from app import db
 
-class commentTest(unittest.TestCase)
+class commentTest(unittest.TestCase):
+  def setUp(self):
+    self.new_pitch=Pitch
